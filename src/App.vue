5e6e0013eeed6eb7dc7b8cd7625bcde1af7de85b
Vgnttv <o.vignani@gmail.com>
2019-07-31 19:28:36 +0200

    Added footer

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,37 @@
 <template>
   <div id="app">
     <Calculator/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Calculator from './components/Calculator.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'app',
   components: {
-    Calculator
+    Calculator,
+    Footer
   }
 }
 </script>
 
 <style>
+html { 
+  background-color: #80b2e4;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 100px;
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+  <footer>
+    <div class="Credits"> © Ottavia Vignani</div>
+  </footer>
+</template>
+<style scoped>
+footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 90px;
+  background-color: #2c3e50;
+  color: white;
+  transition-timing-function: ease-in-out;
+  transition: height 1s;
+  align-items: center;
+  display: flex;
+  justify-content:flex-end;
+  
+}
+.Credits {
+  transition-timing-function: ease-in-out;
+  transition: font-size 1s;
+  margin: 20px;
+  
+}
+footer:hover {
+  height: 200px;
+}
+footer:hover > .Credits {
+  font-size: 60px;
+}
+</style>
