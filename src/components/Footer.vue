84dfb962c19e79b26cb665841e001ef86533c5f3
Vgnttv <o.vignani@gmail.com>
2019-07-31 20:25:26 +0200

    Added linkedin logo and link to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,7 @@
 <template>
   <footer>
-    <div class="Credits"> © Ottavia Vignani</div>
+    <a href="https://www.linkedin.com/in/ottavia-vignani/" class="fa fa-linkedin" target="_blank"></a>
+    <div class="Credits"> © Ottavia Vignani</div> 
   </footer>
 </template>
 <style scoped>
@@ -9,7 +10,7 @@ footer {
   left: 0;
   bottom: 0;
   width: 100%;
-  height: 90px;
+  height: 50px;
   background-color: #2c3e50;
   color: white;
   transition-timing-function: ease-in-out;
@@ -23,12 +24,29 @@ footer {
   transition-timing-function: ease-in-out;
   transition: font-size 1s;
   margin: 20px;
-  
 }
 footer:hover {
-  height: 200px;
+  height: 100px;
 }
 footer:hover > .Credits {
-  font-size: 60px;
+  font-size: 40px;
+}
+.fa {
+  padding: 0.66rem;
+  font-size: 0.66rem;
+  width: 0.66rem;
+  text-align: center;
+  text-decoration: none;
+  margin: 0.16rem 0.06rem;
+  border-radius: 100%;
+}
+
+.fa:hover {
+    opacity: 0.7;
+}
+
+.fa-linkedin {
+  background: rgb(0,123,181);
+  color: white;
 }
 </style>
